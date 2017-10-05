6e28b04aa4a6550ee1ec9e44a7602dba6f2f79a4
Dani Pasic <danipasic@hotmail.com>
2017-10-05 14:13:27 +0200

    Update results after updating match

--- TennisWithMe-WebApi/Services/MatchesServiceImpl.cs
+++ TennisWithMe-WebApi/Services/MatchesServiceImpl.cs
@@ -91,20 +91,64 @@ namespace TennisWithMe_WebApi.Services
         public async Task UpdateMatch(MatchViewModel model)
         {
             using (var db = new ApplicationDbContext())
+            using (var transaction = db.Database.BeginTransaction())
             {
-                await Task.Run(() =>
+                try
                 {
-                    var targetMatch = GetMatches(db).SingleOrDefault(x => x.Id == model.Id);
+                    await Task.Run(() =>
+                    {
+                        // Update match
+                        var targetMatch = GetMatches(db).SingleOrDefault(x => x.Id == model.Id);
 
-                    targetMatch.CityPlayed = model.CityPlayed;
-                    targetMatch.Comment = model.CityPlayed;
-                    targetMatch.Rating = model.Rating;
-                    targetMatch.Result = model.Result;
-                    targetMatch.TimestampPlayed = model.TimestampPlayed;
+                        targetMatch.CityPlayed = model.CityPlayed;
+                        targetMatch.Comment = model.CityPlayed;
+                        targetMatch.Rating = model.Rating;
+                        targetMatch.Result = model.Result;
+                        targetMatch.WinnerId = model.WinnerId;
+                        targetMatch.TimestampPlayed = model.TimestampPlayed;
 
-                    db.SaveChanges();
-                });
+                        db.SaveChanges();
+
+                        // Update players results
+                        var confirmedMatchesChallenger = GetMatches(db).Where(x => (x.ChallengerId == model.ChallengerId || x.OpponentId == model.ChallengerId) && x.IsConfirmed).ToList();
+                        var confirmedMatchesOpponent = GetMatches(db).Where(x => (x.ChallengerId == model.OpponentId || x.OpponentId == model.OpponentId) && x.IsConfirmed).ToList();
+
+                        var challenger = db.Users.SingleOrDefault(x => x.Id == model.ChallengerId);
+                        var opponent = db.Users.SingleOrDefault(x => x.Id == model.OpponentId);
+
+                        UpdateResultsForPlayer(challenger, confirmedMatchesChallenger);
+                        UpdateResultsForPlayer(opponent, confirmedMatchesOpponent);
+
+                        db.SaveChanges();
+
+                        transaction.Commit();
+                    });
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
             }
+        }
+
+        private void UpdateResultsForPlayer(Player player, List<Match> confirmedMatches)
+        {
+            int wons = 0, loses = 0;
+            foreach (var match in confirmedMatches)
+            {
+                if (match.WinnerId == player.Id)
+                {
+                    wons++;
+                }
+                else if (match.WinnerId != null)
+                {
+                    loses++;
+                }
+            }
+
+            player.PlayedGames = wons + loses;
+            player.WonGames = wons;
         }
     }
 }
